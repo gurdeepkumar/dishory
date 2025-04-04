abc0fe098f56ce62f8599ba197c074d888569209
Gurdeep kumar <gurdeepkumar1993@hotmail.com>
2025-04-04 17:12:46 +0100

    Add: base theme and navbar

---
+++ theme/static_src/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      './templates/**/*.{html,js}',
+      './**/templates/**/*.{html,js}',
+      './**/*.html',
+      './**/*.py', // optional, for classnames in Python
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
